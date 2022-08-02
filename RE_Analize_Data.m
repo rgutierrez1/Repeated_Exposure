7277b8b116c676b310f85cb8058b1076df562afe
rodrigo <rodrigo@10.0.0.199>
2022-08-02 12:36:23 -0400

    Editions

--- RE_Analize_Data.m
+++ RE_Analize_Data.m
@@ -1,2 +1,20 @@
 %% RE_Analize_Data
 % Load csv file from Samuel
+
+clear variables; clc;
+addpath(genpath('/Users/rodrigo/MATLAB_Repository/Repeated_Exposure'));
+cd '/Users/rodrigo/MATLAB_Repository/Repeated_Exposure';
+
+%%
+
+T = readtable("waves_newtimes.csv");
+
+%%
+
+idx = T.Sujeto == 1 & T.Sesi_n == 5;
+newTbl = T(idx,:);
+
+%%
+EEG_parameters = newTbl(:,"alpha_max":end);
+
+Mean_Parameters = mean(newTbl,1);
